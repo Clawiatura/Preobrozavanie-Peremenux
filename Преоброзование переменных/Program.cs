16a557afb603b1ebea75c5ad39b6eca6574cef52
Clawiatura <maksimov19992805@icloud.com>
2024-01-19 13:01:30 +0200

    Preobrozovanie 28

--- Преоброзование переменных/Program.cs
+++ Преоброзование переменных/Program.cs
@@ -1,1 +1,12 @@
-﻿ЯЧЧЯ
+﻿string b = "28";
+byte c = byte.Parse(b);
+nint d = (nint)c;
+int f = (int)d;
+long l = (long)f;
+double z = (double)l;
+Console.WriteLine($"{b} {c} {d} {f} {l} {z}");
+
+
+
+
+
